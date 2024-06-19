4bafc1c8d9aabd9fbdf478107dcb074e543e90f9
Emmanuel Tar <emmanueltar14@gmail.com>
2024-06-19 14:55:03 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings:["Python Developer","FullStack Developer","Mobile Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
